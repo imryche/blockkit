f394a1b6dec03bd3b693ce89a1a2a3ada41a9d75
oneor0 <oneor00@gmail.com>
2019-12-08 18:39:18 +0300

    better formating

--- blockkit/components.py
+++ blockkit/components.py
@@ -6,26 +6,32 @@ from .fields import Field
 class ComponentMeta(type):
     def __new__(cls, name, bases, attrs, **kwargs):
         new_class = super().__new__(cls, name, bases, attrs, **kwargs)
-        fields = OrderedDict((key, attrs[key]) for key in attrs if issubclass(
-            attrs[key].__class__, Field))
+        fields = OrderedDict(
+            (key, attrs[key])
+            for key in attrs
+            if issubclass(attrs[key].__class__, Field)
+        )
         new_class._fields = fields
+
         return new_class
 
 
 class Component(metaclass=ComponentMeta):
     def __init__(self, *args):
         if len(args) > len(self._fields):
-            raise IndexError('Number of args exceeds number of fields')
+            raise IndexError("Number of args exceeds number of fields")
 
         fields_iter = iter(self._fields)
 
         for value in args:
             field_name = next(fields_iter)
             field = self._fields[field_name]
+
             if value is not None:
                 value = field.validate(value)
-                validate = getattr(self, f'validate_{field_name}', None)
+                validate = getattr(self, f"validate_{field_name}", None)
                 validate(value) if validate else None
+
             setattr(self, field_name, value)
 
     def build(self):
