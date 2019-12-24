2790e46879d91fdc003c8f451d5abf243b6be275
oneor0 <oneor00@gmail.com>
2019-12-24 20:47:34 +0300

    add MarkdownText and PlainText as top level imports

--- blockkit/__init__.py
+++ blockkit/__init__.py
@@ -1,2 +1,2 @@
-from .objects import Confirm, Option, OptionGroup, Text
+from .objects import Confirm, Option, OptionGroup, Text, MarkdownText, PlainText
 from .elements import Button, DatePicker, Image, MultiSelect
